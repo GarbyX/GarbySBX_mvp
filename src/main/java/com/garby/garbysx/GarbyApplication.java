2e579f43e877bce1d1f08b9538181a2855da7ed4
Gabriel.Madichie <amadichie@yahoo.com>
2022-03-08 10:51:11 +0100

    3rd commit: - Changed port to 8075 - converted application.properties file to a .yaml file - implemented spring profile(s) - Added API documentation http://localhost:8075/swagger-ui.html

--- src/main/java/com/garby/garbysx/GarbyApplication.java
+++ src/main/java/com/garby/garbysx/GarbyApplication.java
@@ -2,12 +2,14 @@ package com.garby.garbysx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+
 public class GarbyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GarbyApplication.class, args);
 	}
-	
+
 }
